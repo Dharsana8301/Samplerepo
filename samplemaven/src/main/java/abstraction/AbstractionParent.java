103100df1ceb16cff1d8be78b83faa65ffef621e
Dharsana <Dharsana@LAPTOP-35G13HI9>
2026-03-24 07:39:30 +0530

    24th March 2026

---
+++ samplemaven/src/main/java/abstraction/AbstractionParent.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public abstract class AbstractionParent {
+	
+	public abstract void display();
+	public abstract void print();
+	public void sum() {
+		int a=10;
+		int b=5;
+		int sum= a+b;
+		System.out.println(sum);
+	}
+
+}
